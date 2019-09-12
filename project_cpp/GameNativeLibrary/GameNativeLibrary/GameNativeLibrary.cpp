390662c278bd7a461ba340720174693a602e7d84
liyunpeng_home <highfly029@gmail.com>
2019-09-13 02:01:44 +0800

    jni

---
+++ project_cpp/GameNativeLibrary/GameNativeLibrary/GameNativeLibrary.cpp
@@ -0,0 +1,27 @@
+// project_cpp.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "GameNativeLibrary.h"
+#include "jni.h"
+#include <iostream>
+
+using namespace std;
+
+JNIEXPORT jstring JNICALL Java_com_highfly029_GameNativeLibrary_testJni
+  (JNIEnv *env, jclass cls, jstring j_str)
+{
+	cout << "Hello Jni" << endl;
+	const char *c_str = NULL;
+    char buff[128] = { 0 };
+    jboolean isCopy;
+    c_str = env->GetStringUTFChars(j_str, &isCopy);
+    if (c_str == NULL)
+    {
+        printf("out of memory.\n");
+        return NULL;
+    }
+    printf("Java Str:%x %s %d %d\n", c_str, c_str, strlen(c_str), isCopy);
+    sprintf_s(buff, "hello %s", c_str);
+    env->ReleaseStringUTFChars(j_str, c_str);
+    return env->NewStringUTF(buff);
+}
--- project_java/src/main/java/com/highfly029/GameNativeLibrary.java
+++ project_java/src/main/java/com/highfly029/GameNativeLibrary.java
@@ -7,4 +7,16 @@ package com.highfly029;
  * @Date 2019/9/12 18:37
  **/
 public class GameNativeLibrary {
+    static {
+        String path = "D:\\project\\GameNativeLibrary\\project_java\\src\\main\\java\\";
+        System.load(path + "GameNativeLibrary.dll");
+    }
+
+    public static native String testJni(String name);
+
+    public static void main(String[] args) {
+        String name = "jni";
+        String text = testJni(name);
+        System.out.println("text=" + text);
+    }
 }
