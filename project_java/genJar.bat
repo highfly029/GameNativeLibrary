aeb923396d26596ef5becebae19bf7dc4209a72e
liyunpeng_work <highfly029@gmail.com>
2019-09-16 20:00:48 +0800

    java

---
+++ project_java/genJar.bat
@@ -0,0 +1,3 @@
+@echo off
+jar cvf GameNativeLibrary.jar -C target/classes .
+pause
--- project_java/src/main/java/com/highfly029/GameNativeLibrary.java
+++ project_java/src/main/java/com/highfly029/GameNativeLibrary.java
@@ -1,5 +1,12 @@
 package com.highfly029;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * @ClassName GameNativeLibrary
  * @Description GameNativeLibrary
@@ -8,9 +15,41 @@ package com.highfly029;
  **/
 public class GameNativeLibrary {
     static {
-        System.out.println(System.getProperty("user.dir"));//user.dir指定了当前的路径
-        String path = System.getProperty("user.dir") + "\\src\\main\\java\\";
-        System.load(path + "GameNativeLibrary.dll");
+        try {
+            boolean isWindows = System.getProperty("os.name", "").toLowerCase().contains("windows");
+
+            String libName = "GameNativeLibrary";
+            String suffix = "";
+            if (isWindows) {
+                suffix += ".dll";
+            } else {
+                suffix += ".so";
+            }
+
+            URL libUrl = Thread.currentThread().getContextClassLoader().getResource(libName + suffix);
+            URLConnection resConn = libUrl.openConnection();
+            resConn.setUseCaches(false);
+            InputStream inputStream = resConn.getInputStream();
+            if (inputStream == null) {
+                System.out.println("cant find GameNativeLibrary.jar");
+            }
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int n;
+            while (-1 != (n = inputStream.read(buffer))) {
+                out.write(buffer, 0, n);
+            }
+
+            File f = File.createTempFile(libName, suffix);
+            FileOutputStream fileOutputStream = new FileOutputStream(f);
+            fileOutputStream.write(out.toByteArray());
+            fileOutputStream.close();
+            System.load(f.getAbsolutePath());
+            System.out.println("load GameNativeLibrary.jar success");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static native String testJni(String name);
