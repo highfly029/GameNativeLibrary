77081647678d9a9211854eded689b8c73a88e8df
aliyun <highfly029@163.com>
2019-09-17 10:55:26 +0800

    test

--- project_cpp/GameNativeLibrary/sh/create_so.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ./GameNativeLibrary.so
-
-g++ -g -O2 -Wall -msse -msse2 -ffast-math  -mfpmath=sse -fPIC --shared -lrt -Wl,-E -I../GameNativeLibrary -o ./GameNativeLibrary.so -I/usr/java/jdk1.8.0_181-amd64/include -I/usr/java/jdk1.8.0_181-amd64/include/linux
