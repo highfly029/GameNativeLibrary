44b3781cb61839b77fa1e269a5549a1f35ca0302
liyunpeng_work <highfly029@gmail.com>
2019-09-18 16:01:16 +0800

    cpp

--- project_cpp/GameNativeLibrary/GameNativeLibrary/GameNativeLibrary.cpp
+++ project_cpp/GameNativeLibrary/GameNativeLibrary/GameNativeLibrary.cpp
@@ -45,10 +45,31 @@ JNIEXPORT jint JNICALL Java_com_highfly029_GameNativeLibrary_load
 * Signature: (Ljava/lang/String;[B)I
 */
 JNIEXPORT jint JNICALL Java_com_highfly029_GameNativeLibrary_load2
-(JNIEnv *, jobject, jstring, jbyteArray)
+(JNIEnv * env, jobject jObj, jstring, jbyteArray jbyteArray)
 {
-	printf("Java_com_highfly029_GameNativeLibrary_load2");
-	return 0;
+	navMeshIdIncre++;
+	int navMeshId = navMeshIdIncre;
+
+	jbyte* olddata = (jbyte*)env->GetByteArrayElements(jbyteArray, 0);
+
+	jsize length = env->GetArrayLength(jbyteArray);
+	NavMesh* nav = new NavMesh();
+	bool suc = nav->create_core((uint8*)olddata, length);
+
+
+	if (!suc) {
+		printf("create failed\n");
+		delete nav;
+		return jint(0);
+	}
+	else {
+		printf("create success\n");
+		meshHandlers[navMeshId] = nav;
+	}
+
+
+	env->ReleaseByteArrayElements(jbyteArray, olddata, 0);
+	return jint(navMeshId);
 }
 
 /*
@@ -57,9 +78,55 @@ JNIEXPORT jint JNICALL Java_com_highfly029_GameNativeLibrary_load2
 * Signature: (IFFFFFF)Ljava/util/List;
 */
 JNIEXPORT jobject JNICALL Java_com_highfly029_GameNativeLibrary_find
-(JNIEnv *, jobject, jint, jfloat, jfloat, jfloat, jfloat, jfloat, jfloat)
+(JNIEnv * env, jobject jobj, jint navMeshId, jfloat x1, jfloat y1, jfloat z1, jfloat x2, jfloat y2, jfloat z2)
 {
-	printf("Java_com_highfly029_GameNativeLibrary_find");
+	std::tr1::unordered_map<int, NavMesh*>::iterator iter = meshHandlers.find(navMeshId);
+	if (iter == meshHandlers.end())
+	{
+		return NULL;
+	}
+	NavMesh* nav = iter->second;
+
+	float start[3];
+	start[0] = x1;
+	start[1] = y1;
+	start[2] = z1;
+
+	float end[3];
+	end[0] = x2;
+	end[1] = y2;
+	end[2] = z2;
+
+
+	std::vector<Position3D> outPaths;
+
+	int ret = nav->findStraightPath(start, end, outPaths);
+
+	if (ret > 0) {
+		jclass class_arraylist = env->FindClass("java/util/ArrayList");
+		jmethodID hashmap_construct_method = env->GetMethodID(class_arraylist, "<init>", "()V");
+		jobject obj_hashmap = env->NewObject(class_arraylist, hashmap_construct_method, "");
+
+		jmethodID arraylist_add_method = env->GetMethodID(class_arraylist, "add", "(Ljava/lang/Object;)Z");
+		int m = 0;
+		std::vector<Position3D>::iterator iter = outPaths.begin();
+		for (; iter != outPaths.end(); ++iter)
+		{
+			jfloat tmp[3];
+			tmp[0] = iter->x;
+			tmp[1] = iter->y;
+			tmp[2] = iter->z;
+			jfloatArray args = (env)->NewFloatArray(3);
+			env->SetFloatArrayRegion(args, 0, 3, tmp);
+
+			env->CallBooleanMethod(obj_hashmap, arraylist_add_method, args);
+
+			m++;
+		}
+
+		return obj_hashmap;
+	}
+
 	return NULL;
 }
 
@@ -69,9 +136,55 @@ JNIEXPORT jobject JNICALL Java_com_highfly029_GameNativeLibrary_find
 * Signature: (IFFFFFF)Ljava/util/List;
 */
 JNIEXPORT jobject JNICALL Java_com_highfly029_GameNativeLibrary_rayCast
-(JNIEnv *, jobject, jint, jfloat, jfloat, jfloat, jfloat, jfloat, jfloat)
+(JNIEnv * env, jobject jobj, jint navMeshId, jfloat x1, jfloat y1, jfloat z1, jfloat x2, jfloat y2, jfloat z2)
 {
-	printf("Java_com_highfly029_GameNativeLibrary_rayCast");
+	std::tr1::unordered_map<int, NavMesh*>::iterator iter = meshHandlers.find(navMeshId);
+	if (iter == meshHandlers.end())
+	{
+		return NULL;
+	}
+	NavMesh* nav = iter->second;
+
+	float start[3];
+	start[0] = x1;
+	start[1] = y1;
+	start[2] = z1;
+
+	float end[3];
+	end[0] = x2;
+	end[1] = y2;
+	end[2] = z2;
+
+
+	std::vector<Position3D> hitPoints;
+
+	int ret = nav->raycast(start, end, hitPoints);
+
+	if (ret == 1) {
+		jclass class_arraylist = env->FindClass("java/util/ArrayList");
+		jmethodID hashmap_construct_method = env->GetMethodID(class_arraylist, "<init>", "()V");
+		jobject obj_hashmap = env->NewObject(class_arraylist, hashmap_construct_method, "");
+
+		jmethodID arraylist_add_method = env->GetMethodID(class_arraylist, "add", "(Ljava/lang/Object;)Z");
+		int m = 0;
+		std::vector<Position3D>::iterator iter = hitPoints.begin();
+		for (; iter != hitPoints.end(); ++iter)
+		{
+			jfloat tmp[3];
+			tmp[0] = iter->x;
+			tmp[1] = iter->y;
+			tmp[2] = iter->z;
+			jfloatArray args = (env)->NewFloatArray(3);
+			env->SetFloatArrayRegion(args, 0, 3, tmp);
+
+			env->CallBooleanMethod(obj_hashmap, arraylist_add_method, args);
+
+			m++;
+		}
+
+		return obj_hashmap;
+	}
+
 	return NULL;
 }
 
@@ -81,8 +194,14 @@ JNIEXPORT jobject JNICALL Java_com_highfly029_GameNativeLibrary_rayCast
 * Signature: (I)I
 */
 JNIEXPORT jint JNICALL Java_com_highfly029_GameNativeLibrary_release
-(JNIEnv *, jobject, jint)
+(JNIEnv *, jobject, jint navMeshId)
 {
-	printf("Java_com_highfly029_GameNativeLibrary_release");
+	std::tr1::unordered_map<int, NavMesh*>::iterator iter = meshHandlers.find(navMeshId);
+	if (iter != meshHandlers.end())
+	{
+		NavMesh* navMesh = iter->second;
+		meshHandlers.erase(iter);
+		delete navMesh;
+	}
 	return 0;
 }
