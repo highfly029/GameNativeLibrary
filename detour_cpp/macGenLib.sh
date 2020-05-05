226db023c972498fe5836604eb3e457dab33a13b
highfly029MacOS <highfly029@gmail.com>
2020-05-05 17:49:07 +0800

    jni

--- detour_cpp/macGenLib.sh
+++ detour_cpp/macGenLib.sh
@@ -1,21 +1,21 @@
 #/usr/bin/bash
 
 clang -c fastlz.c
-clang++ -std=c++17 -stdlib=libc++ -c ChunkyTriMesh.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourAlloc.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourAssert.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourCommon.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourNavMesh.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourNavMeshBuilder.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourNavMeshQuery.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourNode.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourTileCache.cpp
-clang++ -std=c++17 -stdlib=libc++ -c DetourTileCacheBuilder.cpp
-clang++ -std=c++17 -stdlib=libc++ -c MeshLoaderObj.cpp
-clang++ -std=c++17 -stdlib=libc++ -c SoloMesh.cpp
-clang++ -std=c++17 -stdlib=libc++ -c TempObstacle.cpp
+clang++ -std=c++11 -stdlib=libc++ -c ChunkyTriMesh.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourAlloc.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourAssert.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourCommon.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourNavMesh.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourNavMeshBuilder.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourNavMeshQuery.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourNode.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourTileCache.cpp
+clang++ -std=c++11 -stdlib=libc++ -c DetourTileCacheBuilder.cpp
+clang++ -std=c++11 -stdlib=libc++ -c MeshLoaderObj.cpp
+clang++ -std=c++11 -stdlib=libc++ -c SoloMesh.cpp
+clang++ -std=c++11 -stdlib=libc++ -c TempObstacle.cpp
 
-clang++ -std=c++17 -stdlib=libc++ -c main.cpp
+clang++ -std=c++11 -stdlib=libc++ -c main.cpp
 
 # clang++ -o main fastlz.o ChunkyTriMesh.o DetourAlloc.o DetourAssert.o DetourCommon.o DetourNavMesh.o DetourNavMeshBuilder.o DetourNavMeshQuery.o \
 #                 DetourNode.o DetourTileCache.o DetourTileCacheBuilder.o MeshLoaderObj.o SoloMesh.o TempObstacle.o main.o
