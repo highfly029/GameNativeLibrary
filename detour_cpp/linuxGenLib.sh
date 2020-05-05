ced6cdce20c8e0b8472bd88f74b3f2266176249c
highfly029MacOS <highfly029@gmail.com>
2020-05-05 17:51:33 +0800

    jni

--- detour_cpp/linuxGenLib.sh
+++ detour_cpp/linuxGenLib.sh
@@ -1,21 +1,21 @@
 #/usr/bin/bash
 
 gcc -c fastlz.c
-g++ -std=c++11 -stdlib=libc++ -c ChunkyTriMesh.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourAlloc.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourAssert.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourCommon.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourNavMesh.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourNavMeshBuilder.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourNavMeshQuery.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourNode.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourTileCache.cpp
-g++ -std=c++11 -stdlib=libc++ -c DetourTileCacheBuilder.cpp
-g++ -std=c++11 -stdlib=libc++ -c MeshLoaderObj.cpp
-g++ -std=c++11 -stdlib=libc++ -c SoloMesh.cpp
-g++ -std=c++11 -stdlib=libc++ -c TempObstacle.cpp
+g++ -std=c++11 -c ChunkyTriMesh.cpp
+g++ -std=c++11 -c DetourAlloc.cpp
+g++ -std=c++11 -c DetourAssert.cpp
+g++ -std=c++11 -c DetourCommon.cpp
+g++ -std=c++11 -c DetourNavMesh.cpp
+g++ -std=c++11 -c DetourNavMeshBuilder.cpp
+g++ -std=c++11 -c DetourNavMeshQuery.cpp
+g++ -std=c++11 -c DetourNode.cpp
+g++ -std=c++11 -c DetourTileCache.cpp
+g++ -std=c++11 -c DetourTileCacheBuilder.cpp
+g++ -std=c++11 -c MeshLoaderObj.cpp
+g++ -std=c++11 -c SoloMesh.cpp
+g++ -std=c++11 -c TempObstacle.cpp
 
-g++ -std=c++11 -stdlib=libc++ -c main.cpp
+g++ -std=c++11 -c main.cpp
 
 # g++ -o main fastlz.o ChunkyTriMesh.o DetourAlloc.o DetourAssert.o DetourCommon.o DetourNavMesh.o DetourNavMeshBuilder.o DetourNavMeshQuery.o \
 #                 DetourNode.o DetourTileCache.o DetourTileCacheBuilder.o MeshLoaderObj.o SoloMesh.o TempObstacle.o main.o
