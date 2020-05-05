13893aff58a3fb0f986a3f1ed8894a4540619001
aliyun <highfly029@163.com>
2020-05-05 18:01:36 +0800

    jni

--- detour_cpp/linuxGenLib.sh
+++ detour_cpp/linuxGenLib.sh
@@ -1,27 +1,27 @@
 #/usr/bin/bash
 
 gcc -c fastlz.c
-g++ -std=c++11 -c ChunkyTriMesh.cpp
-g++ -std=c++11 -c DetourAlloc.cpp
-g++ -std=c++11 -c DetourAssert.cpp
-g++ -std=c++11 -c DetourCommon.cpp
-g++ -std=c++11 -c DetourNavMesh.cpp
-g++ -std=c++11 -c DetourNavMeshBuilder.cpp
-g++ -std=c++11 -c DetourNavMeshQuery.cpp
-g++ -std=c++11 -c DetourNode.cpp
-g++ -std=c++11 -c DetourTileCache.cpp
-g++ -std=c++11 -c DetourTileCacheBuilder.cpp
-g++ -std=c++11 -c MeshLoaderObj.cpp
-g++ -std=c++11 -c SoloMesh.cpp
-g++ -std=c++11 -c TempObstacle.cpp
+g++ -std=c++11 -fPIC -c ChunkyTriMesh.cpp
+g++ -std=c++11 -fPIC -c DetourAlloc.cpp
+g++ -std=c++11 -fPIC -c DetourAssert.cpp
+g++ -std=c++11 -fPIC -c DetourCommon.cpp
+g++ -std=c++11 -fPIC -c DetourNavMesh.cpp
+g++ -std=c++11 -fPIC -c DetourNavMeshBuilder.cpp
+g++ -std=c++11 -fPIC -c DetourNavMeshQuery.cpp
+g++ -std=c++11 -fPIC -c DetourNode.cpp
+g++ -std=c++11 -fPIC -c DetourTileCache.cpp
+g++ -std=c++11 -fPIC -c DetourTileCacheBuilder.cpp
+g++ -std=c++11 -fPIC -c MeshLoaderObj.cpp
+g++ -std=c++11 -fPIC -c SoloMesh.cpp
+g++ -std=c++11 -fPIC -c TempObstacle.cpp
 
-g++ -std=c++11 -c main.cpp
+g++ -std=c++11 -fPIC -c main.cpp
 
 # g++ -o main fastlz.o ChunkyTriMesh.o DetourAlloc.o DetourAssert.o DetourCommon.o DetourNavMesh.o DetourNavMeshBuilder.o DetourNavMeshQuery.o \
 #                 DetourNode.o DetourTileCache.o DetourTileCacheBuilder.o MeshLoaderObj.o SoloMesh.o TempObstacle.o main.o
 
-g++ -dynamiclib -o libGameNativeLibrary.dylib com_highfly029_GameNativeLibrary.cpp fastlz.o ChunkyTriMesh.o DetourAlloc.o DetourAssert.o DetourCommon.o DetourNavMesh.o DetourNavMeshBuilder.o DetourNavMeshQuery.o \
+g++ -fPIC -shared -o libGameNativeLibrary.so com_highfly029_GameNativeLibrary.cpp fastlz.o ChunkyTriMesh.o DetourAlloc.o DetourAssert.o DetourCommon.o DetourNavMesh.o DetourNavMeshBuilder.o DetourNavMeshQuery.o \
         DetourNode.o DetourTileCache.o DetourTileCacheBuilder.o MeshLoaderObj.o SoloMesh.o TempObstacle.o \
-        -framework JavaVM -I/$JAVA_HOME/include -I/$JAVA_HOME/include/darwin
+        -I/$JAVA_HOME/include -I/$JAVA_HOME/include/linux
 
 rm -rf *.o
